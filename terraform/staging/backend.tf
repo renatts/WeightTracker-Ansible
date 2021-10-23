45adbe8ffe31642d6cd06886d0a7971067eebc08
Renat <paren.nut@gmail.com>
2021-10-23 17:58:30 +0300

    added backend.tf (terraform)

---
+++ terraform/staging/backend.tf
@@ -0,0 +1,9 @@
+## Insert the storage name after storage account is created!!! 
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-weighttracker-staging"
+    storage_account_name = "<storage-account-name>" 
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
